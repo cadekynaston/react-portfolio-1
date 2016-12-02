08e42fabfa150fca868b9ef083f606c68edae65b
Mike <mikeytown19@gmail.com>
2016-12-02 11:54:31 -0700

    skills shit

--- hello-world/src/Skills/skills.js
+++ hello-world/src/Skills/skills.js
@@ -1,108 +1,111 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Scroll from 'react-scroll';
 import Portfolio from '../Portfolio/portfolio.js'
 
-var Link       = Scroll.Link;
+var Link = Scroll.Link;
 var DirectLink = Scroll.DirectLink;
-var Element    = Scroll.Element;
-var Events     = Scroll.Events;
-var scroll     = Scroll.animateScroll;
-var scrollSpy  = Scroll.scrollSpy;
+var Element = Scroll.Element;
+var Events = Scroll.Events;
+var scroll = Scroll.animateScroll;
+var scrollSpy = Scroll.scrollSpy;
 
 var durationFn = function(deltaTop) {
     return deltaTop;
 };
 
-
 class Skills extends Component {
     componentDidMount() {
 
-    Events.scrollEvent.register('begin', function(to, element) {
-      console.log("begin", arguments);
-    });
+        Events.scrollEvent.register('begin', function(to, element) {
+            console.log("begin", arguments);
+        });
 
-    Events.scrollEvent.register('end', function(to, element) {
-      console.log("end", arguments);
-    });
+        Events.scrollEvent.register('end', function(to, element) {
+            console.log("end", arguments);
+        });
 
-    scrollSpy.update();
+        scrollSpy.update();
 
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove('begin');
-    Events.scrollEvent.remove('end');
-  }
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-  scrollToBottom() {
-    scroll.scrollToBottom();
-  }
-  scrollTo() {
-    scroll.scrollTo(100);
-  }
-  scrollMore() {
-    scroll.scrollMore(100);
-  }
-  handleSetActive(to) {
-    console.log(to);
-  }
-  render() {
-    return (
-        <div>
-            <div className="text-align-center caps fw900 i-like">
-                Skills
-            </div>
-            <div className="skills-description text-align-center fw300">Some of my favorite frameworks / environments. </div>
+    }
+    componentWillUnmount() {
+        Events.scrollEvent.remove('begin');
+        Events.scrollEvent.remove('end');
+    }
+    scrollToTop() {
+        scroll.scrollToTop();
+    }
+    scrollToBottom() {
+        scroll.scrollToBottom();
+    }
+    scrollTo() {
+        scroll.scrollTo(100);
+    }
+    scrollMore() {
+        scroll.scrollMore(100);
+    }
+    handleSetActive(to) {
+        console.log(to);
+    }
 
-            <div className="flex jc-center">
-                <div className="flex">
-                    <div className="circle-border ">
-                        <div><i className="fa fa-code fa-1x about-icons fa-custom-circle"></i></div>
-
-
-                        </div>
-                        <div>
-                            <div>JS</div>
-                            <ul>
-                                <li>React</li>
-                                <li>jQuery</li>
-                                <li>Angular</li>
-                                <li>Brackets</li>
-                            </ul>
-                        </div>
-                </div>
-                <div className="flex">
-                    <div><i className="fa fa-desktop fa-1x about-icons"></i></div>
-                    <div>
-                        <div>UI / UX</div>
-                        <ul>
-                            <li>HTML & CSS</li>
-                            <li>Illustrator</li>
-                            <li>Code Pen</li>
-                            <li>Sketch</li>
-                        </ul>
+    render() {
+        return (
+            <div>
+                <div className="skill-margin-top">
+                    <div className="text-align-center caps fw900">
+                        Skills
+                    </div>
+                    <div className="skills-description text-align-center fw300">Some of my favorite frameworks / environments.
                     </div>
                 </div>
-                <div className="flex">
-                    <div><i className="fa fa-rocket fa-1x about-icons"></i></div>
-                    <div>
-                        <div>LAUNCH</div>
-                        <ul>
-                            <li>Mongo</li>
-                            <li>Git Hub</li>
-                            <li>Heroku</li>
-                            <li>Sql</li>
-                        </ul>
-                    </div>
+                <div className="flex-wrap jc-space-around skill-bottom-padding">
+
+
+                        <div className="flex skill-thirds">
+                            <div className="circle-border ">
+                                <i className="fa fa-code fa-1x about-icons fa-custom-circle"></i>
+                            </div>
+                            <div >
+                                <div className="skill-title fw900">JS</div>
+
+                                <li className="sub-skills-bullets fw300">React</li>
+                                <li className="sub-skills-bullets fw300">jQuery</li>
+                                <li className="sub-skills-bullets fw300">Angular</li>
+                                <li className="sub-skills-bullets fw300">Brackets</li>
+                            </div>
+                        </div>
+
+                        <div className="flex skill-thirds">
+                            <div className="circle-border ">
+                                <i className="fa fa-desktop fa-1x about-icons fa-custom-circle"></i>
+                            </div>
+                            <div className="skill-title fw900">
+                                <div className="skill-title fw900">UI / UX</div>
+
+                                <li className="sub-skills-bullets fw300">HTML & CSS</li>
+                                <li className="sub-skills-bullets fw300">Illustrator</li>
+                                <li className="sub-skills-bullets fw300">Code Pen</li>
+                                <li className="sub-skills-bullets fw300">Sketch</li>
+                            </div>
+                        </div>
+                        <div className="flex skill-thirds">
+                            <div className="circle-border ">
+                                <i className="fa fa-rocket fa-1x about-icons fa-custom-circle"></i>
+                            </div>
+                            <div className="skill-title fw900">
+                                <div className="skill-title fw900">LAUNCH</div>
+                                <li className="sub-skills-bullets fw300">Mongo</li>
+                                <li className="sub-skills-bullets fw300">Git Hub</li>
+                                <li className="sub-skills-bullets fw300">Herouku</li>
+                                <li className="sub-skills-bullets fw300">Sql</li>
+                            </div>
+                        </div>
+
+
                 </div>
 
-
             </div>
-
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Skills;
