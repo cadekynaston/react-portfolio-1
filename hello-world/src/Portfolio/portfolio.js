9fc95f519f997173fa3cb162b7126f2cc6678086
Michael Harvey <michaelharvey@Totoro.local>
2016-12-05 09:33:43 -0700

    got the project funtion working with state

--- hello-world/src/Portfolio/portfolio.js
+++ hello-world/src/Portfolio/portfolio.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+
+
 let projects = [
 	{url:'https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/15069004_10155196765331754_4116709789900966839_o.jpg',
 		name:'michael',
@@ -21,32 +24,41 @@ let projects = [
 		src:'',
 	}
 ]
-let sortProjects = function(projects){
-	console.log('In sortProjects');
-	console.log('idiot');
-	console.log(project)
 
-}
 // clicking on ID and apply this keyword to "active" which can hide all others and only show the "active" elements within that Component
 let projectGroups =["all" , "web", "music", "app"];
-let projectSelection = projectGroups.map(function(name, i){
 
-	return <div value={name} className="project-subtext project-buttons app fw900"key={name} onClick={this}>{name} </div>
 
-});
-let project = projects.map(function(data, i ){
 
-	return <div className='flex jc-center' value={data.id} key={i}>
-		<img alt=''className="project-image"src={data.url}/>
-		<p className="fw100 project-subtext text-align-center">{data.name}</p>
-	</div>
-})
 
-console.log(project)
 class Portfolio extends Component {
-	
 
+	constructor(props){
+		super(props)
+		this.state = {filterValue:'all'};
+
+	}
+	 sortProjects(e){
+		 console.log(e.target);
+		 this.setState({filterValue:e.target.dataset.value});
+
+	}
 	render() {
+		let project = projects.filter(data => this.state.filterValue === "all"|| data.id === this.state.filterValue).map(function(data, i ){
+
+			return <div className='flex jc-center' value={data.id} key={i}>
+				<img alt=''className="project-image"src={data.url}/>
+				<p className="fw100 project-subtext text-align-center">{data.name}</p>
+			</div>
+		})
+
+		console.log(this.state.filterValue, "ALL THE LONELY PEOPLE");
+		console.log(this);
+		let projectSelection = projectGroups.map((name, i) => {
+
+			return <div data-value={name} className="project-subtext project-buttons app fw900"key={name} onClick={this.sortProjects.bind(this)}>{name} </div>
+
+		});
 		return (
 			<div>
 				<div className="text-align-center caps fw900">
