34190ba3190c0d25a94ca0266fb775f277aad062
Mike <mikeytown19@gmail.com>
2016-12-13 21:18:35 -0700

    added the styling on the image for the projects

--- hello-world/src/Portfolio/portfolio.js
+++ hello-world/src/Portfolio/portfolio.js
@@ -1,52 +1,36 @@
 import React, { Component } from 'react';
+import projects from './projects.js';
 
-let projects = [
-	{url:'https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/15069004_10155196765331754_4116709789900966839_o.jpg',
-		name:'michael',
-		description:'This is an app',
-		id:'web',
-		src:'',
-	},
-	{url:'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/35845_1300231667680_7755683_n.jpg?oh=d806d86304eb4c1e4938533ee7d9d325&oe=58F38CB0',
-		name:'kelly',
-		description:'This is an app',
-		id:'web',
-		src:'',
-	}
-	,
-	{url:'https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/12240288_1222972601053534_1288458191114782947_o.jpg',
-		name:'cade',
-		description:'This is an app',
-		id:'app',
-		src:'',
-	}
-]
-let sortProjects = function(projects){
-	console.log('In sortProjects');
-	console.log('idiot');
-	console.log(project)
-
-}
 // clicking on ID and apply this keyword to "active" which can hide all others and only show the "active" elements within that Component
 let projectGroups =["all" , "web", "music", "app"];
-let projectSelection = projectGroups.map(function(name, i){
 
-	return <div value={name} className="project-subtext project-buttons app fw900"key={name} onClick={this}>{name} </div>
-
-});
-let project = projects.map(function(data, i ){
-
-	return <div className='flex jc-center' value={data.id} key={i}>
-		<img alt=''className="project-image"src={data.url}/>
-		<p className="fw100 project-subtext text-align-center">{data.name}</p>
-	</div>
-})
-
-console.log(project)
 class Portfolio extends Component {
-	
 
+	constructor(props){
+		super(props)
+		this.state = {filterValue:'all'};
+	}
+	 sortProjects(e){
+		 this.setState({filterValue:e.target.dataset.value});
+	}
 	render() {
+
+
+		let project = projects.filter(data => this.state.filterValue === "all"|| data.id === this.state.filterValue).map(function(data, i ){
+			return <div className='flex-wrap jc-center ' value={data.id} key={i}>
+									<div>
+										<img alt=''className="project-image"src={data.url}/>
+									
+									</div>
+
+							</div>
+						})
+
+		let projectSelection = projectGroups.map((name, i) => {
+
+			return <div data-value={name} className="project-subtext project-buttons app fw900"key={name} onClick={this.sortProjects.bind(this)}>{name} </div>
+
+		});
 		return (
 			<div>
 				<div className="text-align-center caps fw900">
@@ -58,13 +42,11 @@ class Portfolio extends Component {
 				<div className="flex jc-center">
 					{projectSelection}
 				</div>
-
-				<div className="jc-center flex">
+				<div className="jc-center flex-wrap max-width">
 					{project}
 				</div>
 			</div>
 		)
 	}
 }
-
 export default Portfolio;
---
+++ hello-world/src/Portfolio/projects.js
@@ -0,0 +1,43 @@
+
+
+let projects = [
+	{url:'https://mikeytown19.github.io/newPersonal/assets/img/instajam.jpeg',
+		name:'michael',
+		description:'This is an app',
+		id:'web',
+		src:'',
+	},
+	{url:'https://mikeytown19.github.io/newPersonal/assets/img/crossfit.jpeg',
+		name:'kelly',
+		description:'This is an app',
+		id:'web',
+		src:'',
+	}
+	,
+	{url:'https://mikeytown19.github.io/newPersonal/assets/img/twitter.jpeg',
+		name:'cade',
+		description:'This is an app',
+		id:'app',
+		src:'',
+	},
+		{url:'https://mikeytown19.github.io/newPersonal/assets/img/dad.jpeg',
+			name:'kelly',
+			description:'This is an app',
+			id:'web',
+			src:'',
+		}
+		,
+		{url:'https://mikeytown19.github.io/newPersonal/assets/img/japan4.jpeg',
+			name:'cade',
+			description:'This is an app',
+			id:'app',
+			src:'',
+		},{url:'https://mikeytown19.github.io/newPersonal/assets/img/japan3.jpeg',
+				name:'michael',
+				description:'This is an app',
+				id:'web',
+				src:'',
+			}
+]
+
+export default projects;
